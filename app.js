60b657d8402ce07f97f76874de20f0e58412eab9
Malik Turnbull <Malik@maliks-mbp.home>
2020-07-16 19:17:52 -0400

    [add superhero]

---
+++ app.js
@@ -0,0 +1,66 @@
+function greeting(name, callback) {
+    let sentence = 'Hello, my name is ' + name;
+    return sentence;
+}
+function anotherGreeting() {
+    let sentence = 'I forgot my last name';
+    return sentence;
+}
+let myName = 'malik'
+console.log(greeting(myName, anotherGreeting));
+//exercise
+function printArrayAgain(array, callback) {
+    for (let i = 0; i < array.length; i++){
+        let number = array[i];
+        console.log(number);
+    }
+    callback(array);
+}
+    function printArrayAgain(array) {
+        for (let i = 0; i < array.length; i++) {
+            let number = array[i];
+            console.log(number);
+        }
+    }
+    const numbersArray = [1,2,3,4,5,9,8,7,6];
+printArray(numbersArray, printArrayAgain);
+//exercise
+function calculator(num1, num2, callbackOne, callbackTwo){
+    let result = callbackOne(num1, num2) + callbackTwo(num1, num2);
+    return SpeechRecognitionResultList;
+function subtractNumbers(num1, num2) {
+}
+}
+//make a superHero function
+
+//Take in two callbacks
+
+//return a string in each callback describing the superpower
+
+function greeting(name, callback){
+    let sentence= 'I AM' + name;
+    return sentence;
+    
+}
+function anotherGreeting(){
+    let sentence= 'Its pizza time';
+    return sentence;
+}
+
+
+
+// solution
+function spider-man(superpowerOne, superpowerTwo){
+    let allSuperpowers = superpowerOne() + ' and ' +superpowerTwo();
+    return allSuperpowers;
+}
+function webBlaster(){
+    let power = 'Its pizza time!'
+    return power;
+}
+function swingKick(){
+    let kick= 'Its pizza time!';
+    return kick;
+}
+
+console.log(spider-man(webBlaster, swingKick));
